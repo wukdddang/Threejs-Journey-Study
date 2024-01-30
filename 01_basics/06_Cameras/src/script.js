220632a279664420f3a2216789cecd55ffe1aa82
wukdddang <dnr8874@naver.com>
2024-01-30 22:54:21 +0900

    Feat: Three.js Journey 강의 학습 - 6 / 54

--- 01_basics/06_Cameras/src/script.js
+++ 01_basics/06_Cameras/src/script.js
@@ -1,4 +1,19 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+/**
+ * Cursor
+ */
+const cursor = {
+  x: 0,
+  y: 0,
+};
+
+window.addEventListener("mousemove", (event) => {
+  // console.log(event.clientX);
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = event.clientY / sizes.height - 0.5;
+});
 
 /**
  * Base
@@ -23,12 +38,37 @@ const mesh = new THREE.Mesh(
 scene.add(mesh);
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.x = 2;
-camera.position.y = 2;
-camera.position.z = 2;
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+
+// const aspectRatio = sizes.width / sizes.height;
+// console.log(aspectRatio);
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1,
+//   0.1,
+//   100
+// );
+// camera.position.x = 2;
+// camera.position.y = 2;
+camera.position.z = 3;
+// console.log(camera.position.length());
 camera.lookAt(mesh.position);
 scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.enableZoom = true;
+controls.zoomSpeed = 0.5;
+// controls.target.y = 2;
+// controls.update();
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -43,7 +83,16 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
   // Update objects
-  mesh.rotation.y = elapsedTime;
+  // mesh.rotation.y = elapsedTime;
+
+  // Update camera
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  // camera.position.y = cursor.y * 5;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  // camera.lookAt(mesh.position);
+
+  // Update controls
+  controls.update();
 
   // Render
   renderer.render(scene, camera);
