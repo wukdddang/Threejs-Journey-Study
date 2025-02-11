dcb49e0e80db348b2b33aa43b9c0760d0f1332aa
wukddang <dnr8874@naver.com>
2025-02-12 00:27:09 +0900

    Feat: threejs 학습중 - 4강

--- 01_basics/04/src/script.js
+++ 01_basics/04/src/script.js
@@ -1,59 +1,61 @@
 import * as THREE from "three";
 
+// Canvas
 const canvas = document.querySelector("canvas.webgl");
 
-// ? Scene
+// Scene
 const scene = new THREE.Scene();
 
-// ? Axes Helper
+/**
+ * Objects
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+mesh.position.set(0.7, -0.6, 1);
+scene.add(mesh);
+
+// Scale
+// mesh.scale.x = 2;
+// mesh.scale.y = 0.5;
+// mesh.scale.z = 0.5;
+mesh.scale.set(2, 0.5, 0.5);
+
+// Rotation
+// 기본적으로 rotate 순서는 x, y, z 순서로 돌아감
+// 그래서 x 로 먼저 돌리면, y, z 축도 돌아감. 이를 계산해야함
+
+// reorder를 하면 순서를 바꿀 수 있음
+mesh.rotation.reorder("YXZ");
+mesh.rotation.y = Math.PI * 0.25;
+mesh.rotation.x = Math.PI * 0.25;
+
+// Axes Helper: 어려우면 axes 호출해서 위치 감 잡기
 const axesHelper = new THREE.AxesHelper();
 scene.add(axesHelper);
 
-// ? Group
-const group = new THREE.Group();
-group.position.y = 1;
-group.scale.y = 2;
-group.rotation.y = 1;
-scene.add(group);
-
-const cube1 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0xff0000 })
-);
-group.add(cube1);
-const cube2 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-);
-
-cube2.position.x = -2;
-group.add(cube2);
-
-const cube3 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0x0000ff })
-);
-
-cube3.position.x = 2;
-group.add(cube3);
-
-// ? Sizes: Camera의 비율을 정해주기 위한 값
+/**
+ * Sizes
+ */
 const sizes = {
   width: 800,
   height: 600,
 };
 
-// ? Camera
+/**
+ * Camera
+ */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
-// camera.position.y = 1;
-// camera.position.x = 1;
 scene.add(camera);
 
-// ? Renderer
+camera.lookAt(mesh.position);
+
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
-  canvas,
+  canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
-
 renderer.render(scene, camera);
--- 01_basics/04/vite.config.js
+++ 01_basics/04/vite.config.js
@@ -1,7 +1,8 @@
+import restart from 'vite-plugin-restart'
+
 export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
+    root: 'src/', // Sources files (typically where index.html is)
+    publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
     server:
     {
         host: true, // Open to local network and display URL
@@ -13,4 +14,8 @@ export default {
         emptyOutDir: true, // Empty the folder first
         sourcemap: true // Add sourcemap
     },
+    plugins:
+    [
+        restart({ restart: [ '../static/**', ] }) // Restart server on static file change
+    ],
 }
